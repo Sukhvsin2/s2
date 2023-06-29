582e30ab46fd5896c8327c3d9a690630310c4ad5
Sukhvinder Singh <sukhvsingh2026@gmail.com>
2023-06-29 15:27:13 -0400

    done

--- src/app/onlyfans/page.tsx
+++ src/app/onlyfans/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect } from 'react'
 
-export default function onlyfans() {
+export default function onlyfans({count}: any) {
 
   const getCounts = async () => {
     const res = await fetch('/api/counts')
@@ -15,7 +15,7 @@ export default function onlyfans() {
 
   return (
     <main className='text-center pt-4'>
-      <h1>{}</h1>
+      <h1 className='text-2xl text-center pb-2'>{count}</h1>
       <p>people have searched for sukhvinder's OnlyFans page 🥵</p>
     </main>
   )
